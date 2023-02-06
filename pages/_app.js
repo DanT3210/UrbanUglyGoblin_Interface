1c67ddd054732209f68a0e3b9f22061a0e71292d
Dant3210 <daneel1982@gmail.com>
2023-02-05 22:52:57 -0500

    modified:   pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,14 @@ import '../styles/globals.css'
 import {MoralisProvider} from 'react-moralis';
 import Header from '../components/Header';
 import MyFooter from '../components/MyFooter';
-import NonSSRWrapper from '../components/NonSSRWrapper';
+
 
 function MyApp({ Component, pageProps }) {
 
   return(<MoralisProvider initializeOnMount={false}>
-    <NonSSRWrapper>
     <Header/>
      <Component {...pageProps} />
     <MyFooter/> 
-    </NonSSRWrapper>
   </MoralisProvider>)
 }
 
