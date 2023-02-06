9afbb28470058dac41a1e27bb7ea4f53f6e22cfb
Dant3210 <daneel1982@gmail.com>
2023-02-05 23:00:45 -0500

    modified:   pages/navegation/history.js

--- pages/navegation/history.js
+++ pages/navegation/history.js
@@ -2,10 +2,6 @@ import Image from 'next/image'
 //import styles from '../../styles/Home.module.css'
 import { useMoralis, useWeb3Contract } from 'react-moralis';
 import { useState } from 'react';
-import Header from '../../components/Header';
-import MyFooter from '../../components/MyFooter';
-
-
 
 const my_styles = {
   container: `h-full w-[300px] flex flex-col bg-[#fff] static`,
@@ -15,10 +11,10 @@ export default function History() {
   const [accounts, setAccounts]=useState([]); 
   const {enableWeb3, isWeb3Enable}=useMoralis();
   return (<>
-    <Header/>
+
     <div className="h-90v md:flex justify-center items-center">
       History
     </div>
-    <MyFooter/>
+
     </>)
 }
